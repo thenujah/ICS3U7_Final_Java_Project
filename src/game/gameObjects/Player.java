71e2939c05b9210e036446355396697110e7c75a
thenujah <thenujah.ketheeswaran@student.tdsb.on.ca>
2022-01-06 10:47:16 -0500

    Player class created.

---
+++ src/game/gameObjects/Player.java
@@ -0,0 +1,19 @@
+package game.gameObjects;
+
+import java.awt.Graphics2D;
+
+public class Player {
+	
+	
+	
+	
+	
+	public void movement() {
+		
+	}
+	
+	public void render(Graphics2D g) {
+		
+	}
+
+}
